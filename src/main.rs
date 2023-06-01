971280466c6f1882094b5980ed3058ad844a6c7a
Joe <yu.xue.work@outlook.com>
2023-06-01 18:00:05 +0800

    feat: shared state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -7,49 +12,47 @@ async fn main() {
     // 监听指定地址，等待 TCP 连接进来
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // 第二个被忽略的项中包含有新连接的 `IP` 和端口信息
         let (socket, _) = listener.accept().await.unwrap();
         // 为每一条连接都生成一个新的任务，
         // `socket` 的所有权将被移动到新的任务中，并在那里进行处理
+        let db = db.clone();
+
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
 
-    // 使用 hushmap 存储 redis 数据
-    let mut db = HashMap::new();
-
-    // `mini-redis` 提供的便利函数，使用返回的 `connection` 可以用于从 socket 中读取数据并解析为数据帧
     let mut connection = Connection::new(socket);
 
-    // 使用 `read_frame` 方法从连接获取一个数据帧：一条redis命令 + 相应的数据
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // 值被存储为 `Vec<u8>` 的形式
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
-                    // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
             }
             cmd => panic!("unimplemented {:?}", cmd),
         };
-        
-        // 将请求响应返回给客户端
+
         connection.write_frame(&response).await.unwrap();
     }
-    
 }
